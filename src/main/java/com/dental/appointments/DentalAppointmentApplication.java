de9b955924ea7d115185136cff67c0adaa552eb6
peddi <peddipradeep26@gmail.com>
2019-09-11 16:25:03 -0400

    Added additional comments for test check-in and commits

--- src/main/java/com/dental/appointments/DentalAppointmentApplication.java
+++ src/main/java/com/dental/appointments/DentalAppointmentApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication 
 public class DentalAppointmentApplication {
 
+    // Run this method to start SpringBoot application.
     public static void main(String[] args) {
         SpringApplication.run(DentalAppointmentApplication.class, args);
     }
--- src/main/java/com/dental/appointments/service/DentalAppointmentService.java
+++ src/main/java/com/dental/appointments/service/DentalAppointmentService.java
@@ -27,6 +27,7 @@ public class DentalAppointmentService {
     public Appointment getAppointmentById(String id)
     {
         // Production code should never have print statements.
+        // Added additional comment for test check-in
         System.out.println(">>> DentalAppointmentService: getAppointmentById(id) ");
         return appointmentRepository.findById(Integer.parseInt(id)).get();
     }
